ef9859ac8725586f6f6076915461ac818d287114
Mohsen Ali (2211-007-KHI-CND) <mohsen.ali@xloopdigital.com>
2023-02-19 12:51:43 +0500

    Created controller for hotel

---
+++ src/main/java/gul/hotel/HotelController.java
@@ -0,0 +1,26 @@
+package gul.hotel;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/hotels")
+public class HotelController {
+    
+    private HotelService hotelService;
+
+    public HotelController(HotelService hotelService) {
+        this.hotelService = hotelService;
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Hotel createNewHotel(@RequestBody Hotel hotel){
+        return hotelService.saveHotel(hotel);
+    }
+    
+}
--- src/test/java/gul/hotel/HotelControllerTest.java
+++ src/test/java/gul/hotel/HotelControllerTest.java
@@ -1,9 +1,25 @@
 package gul.hotel;
 
+import org.junit.jupiter.api.Test;
+import org.mockito.invocation.Invocation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.BDDMockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
+
+// import static org.mockito.ArgumentMatcher.*;
+// import static org.mockito.BDDMockito.*;
+// import static org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder.*;
+// import static org.springframework.test.web.servlet.result.ContentResultMatchers.*;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
@@ -16,6 +32,33 @@ public class HotelControllerTest {
     @Autowired
     private ObjectMapper objectMapper;
 
+    @Test
+    public void canPostANewHotel() throws Exception{
+        Hotel hotel = Hotel.builder()
+				.hotelName("PC Hotel")
+				.shortDescription("This is Pc hotel")
+				.longDescription("it is long description of Pc hotel")
+				.img("img")
+				.location("Karchi")
+				.experienceLevel("Budget")
+				.pool("No")
+				.price(2356L)
+				.build();
+        given(hotelService.saveHotel(any(Hotel.class))).willAnswer(invocation -> invocation.getArgument(0));
+        ResultActions response = mockMvc.perform(post("/api/hotels")
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(objectMapper.writeValueAsString(hotel))); 
+        response.andDo(print())
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.hotelName", is(hotel.getHotelName())))
+                .andExpect(jsonPath("$.price", is(hotel.getPrice())))  
+                .andExpect(jsonPath("$.id", is(hotel.getId())))
+                .andExpect(jsonPath("$.pool", is(hotel.getPool())))  
+                .andExpect(jsonPath("$.experienceLevel", is(hotel.getExperienceLevel())))
+                .andExpect(jsonPath("$.img", is(hotel.getImg())))  
+                .andExpect(jsonPath("$.location", is(hotel.getLocation())))
+                .andExpect(jsonPath("$.longDescription", is(hotel.getLongDescription())))  
+                .andExpect(jsonPath("$.shortDescription", is(hotel.getShortDescription())));
+    }
 
-    
 }
