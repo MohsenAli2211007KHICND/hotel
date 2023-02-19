84dde985442de1810436a3f56657b6cafd749556
Mohsen Ali (2211-007-KHI-CND) <mohsen.ali@xloopdigital.com>
2023-02-19 15:21:05 +0500

    controller 7th test passed(can delete a hotel)

--- src/main/java/gul/hotel/HotelController.java
+++ src/main/java/gul/hotel/HotelController.java
@@ -2,18 +2,16 @@ package gul.hotel;
 
 import java.util.List;
 
-import javax.net.ssl.SSLEngineResult.Status;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -56,10 +54,13 @@ public class HotelController {
         catch(Exception error){
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new Hotel());
         }
-
-        
-
+}
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<String> deleteHotel(@PathVariable("id") Long id){
+        hotelService.deleteHotel(id);
+        return new ResponseEntity<String>("Hotel deleted successfully!", HttpStatus.OK);
         
     }
     
 }
+
